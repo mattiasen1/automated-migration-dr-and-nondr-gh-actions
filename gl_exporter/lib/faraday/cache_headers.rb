050028e790ec0ae51ddcd9d51f9fbe30e20f8105
pauljaison <79197694+pauljaison@users.noreply.github.com>
2026-04-27 18:11:56 +0530

    Delete gl_exporter directory

--- gl_exporter/lib/faraday/cache_headers.rb
+++
@@ -1,9 +0,0 @@
-module Faraday
-  class CacheHeaders < Faraday::Response::Middleware
-    # A little hack middleware to manually set cache headers. GitLab by default
-    # does not allow caching of its API
-    def on_complete(env)
-      env.response_headers["cache-control"] = "max-age=3600, public"
-    end
-  end
-end
--- gl_exporter/spec/support/gitlab_spec_helpers.rb
+++
@@ -1,8 +0,0 @@
-module GitlabSpecHelpers
-  def with_ssl_verify(value, &block)
-    old_ssl_verify = Gitlab.ssl_verify
-    Gitlab.ssl_verify = value
-    block.call
-    Gitlab.ssl_verify = old_ssl_verify
-  end
-end
